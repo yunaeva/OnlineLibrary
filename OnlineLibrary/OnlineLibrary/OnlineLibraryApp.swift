b0214639b4a9364654efdb5521d451c749ab45dc
Safiya Yunaeva <safiya.yunaeva@gmail.com>
2024-06-22 23:21:09 -0700

    Get infor about available books from free Online library

---
+++ OnlineLibrary/OnlineLibrary/OnlineLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineLibraryApp.swift
+//  OnlineLibrary
+//
+//  Created by syunaeva on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
